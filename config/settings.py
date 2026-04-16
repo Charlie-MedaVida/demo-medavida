48916405f974be9f76651f2c916a2f1443cef288
MedaVida Charlie <charlie@medavida.com>
2026-04-16 11:53:04 -0500

    Add CORS checks

--- config/settings.py
+++ config/settings.py
@@ -67,6 +67,7 @@ AUTHENTICATION_BACKENDS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
