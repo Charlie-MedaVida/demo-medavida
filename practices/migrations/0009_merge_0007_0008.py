f3532792698b3cffad597b468882c92d5f9f3dce
MedaVida Charlie <charlie@medavida.com>
2026-04-17 10:21:33 -0500

    Merge Migrations

---
+++ practices/migrations/0009_merge_0007_0008.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('practices', '0007_providerbypractice_add_uncredentialed_type'),
+        ('practices', '0008_alter_providerbypractice_type'),
+    ]
+
+    operations = [
+    ]
