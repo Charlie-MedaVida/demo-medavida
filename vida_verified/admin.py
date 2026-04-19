f6a5b41867c0e7bdf9103ee7b71e694420020bf8
MedaVida Charlie <charlie@medavida.com>
2026-04-19 17:33:47 -0500

    1

--- vida_verified/admin.py
+++ vida_verified/admin.py
@@ -1,7 +1,12 @@
 from admin_extra_buttons.api import ExtraButtonsMixin, button
 from django.contrib import admin, messages
 
-from .models import DeaCredential, NpiCredential
+from .models import (
+    DeaCredential,
+    DeaVerificationView,
+    NpiCredential,
+    NpiVerificationView,
+)
 
 
 @admin.register(NpiCredential)
@@ -32,3 +37,47 @@ class DeaCredentialAdmin(ExtraButtonsMixin, admin.ModelAdmin):
             'DEA license extraction task queued.',
             messages.SUCCESS,
         )
+
+
+@admin.register(NpiVerificationView)
+class NpiVerificationViewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'license_number', 'verified',
+        'checked_at', 'enumeration_date', 'expiration_date',
+    )
+    readonly_fields = (
+        'id', 'license_number', 'last_checked_at',
+        'enumeration_date', 'expiration_date',
+        'checked_at', 'verified', 'error_content', 'json_content',
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(DeaVerificationView)
+class DeaVerificationViewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'license_number', 'verified',
+        'checked_at', 'enumeration_date', 'expiration_date',
+    )
+    readonly_fields = (
+        'id', 'license_number', 'last_checked_at',
+        'enumeration_date', 'expiration_date',
+        'checked_at', 'verified', 'error_content', 'json_content',
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
